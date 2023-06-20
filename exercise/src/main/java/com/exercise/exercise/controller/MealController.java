1b26d7a4ee6df740e521c79179a6e9a2d6ac358d
Massimiliano Demma <demma.massimiliano@yahoo.it>
2023-06-20 23:17:37 +0200

    Esercizio 3.1

--- exercise/src/main/java/com/exercise/exercise/controller/MealController.java
+++ exercise/src/main/java/com/exercise/exercise/controller/MealController.java
@@ -2,16 +2,18 @@ package com.exercise.exercise.controller;
 
 import com.exercise.exercise.service.Meal;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class MealController {
 
-    List<Meal> mealList = Arrays.asList(
+    List<Meal> mealList = new ArrayList<>();
+
+    List<Meal> mealList1 = Arrays.asList(
             new Meal("Carbonara","Good",8.99),
             new Meal("Bistecca","NotBad",12.56),
             new Meal("Caviale","Delicious",45.8)
@@ -21,7 +23,19 @@ public class MealController {
 
     public ResponseEntity<List<Meal>> getMeallist() {
 
-        return ResponseEntity.ok(mealList);
+        return ResponseEntity.ok(mealList1);
     }
 
+    @PutMapping(value="/meal")
+
+    public ResponseEntity<String>putMeal(@RequestBody Meal meal){
+
+        this.mealList.add(meal);
+        return ResponseEntity.ok("Aggiunto Meal");
+
+    }
+
+
 }
+
+
